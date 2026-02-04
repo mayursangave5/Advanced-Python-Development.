9738b1bf7601cdddf78122478c8642420b579556
mayursangave5 <sangavemayur5@gmail.com>
2026-02-05 00:08:25 +0530

    Task-1

--- Assignment-1/basic_math_operations.py
+++ Assignment-1/basic_math_operations.py
@@ -4,16 +4,16 @@ num2=int(input("enter second numbers :- "))
 
 #ADDITION
 add=num1+num2
-print(f"addition : {num1}+{num2}={add}")
+print(f"addition : {num1}+{num2}={add}\n")
 
 #SUBTRACTION
 sub=num1-num2
-print(f"subtraction : {num1}-{num2}={sub}")
+print(f"subtraction : {num1}-{num2}={sub}\n")
 
 #MULTIPLICATION
 mul=num1*num2
-print(f"multiplication : {num1}*{num2}={mul}")
+print(f"multiplication : {num1}*{num2}={mul}\n")
 
 #DIVISION
 div=num1/num2
-print(f"dividion : {num1}/{num2}={div}")
+print(f"dividion : {num1}/{num2}={div}\n")
