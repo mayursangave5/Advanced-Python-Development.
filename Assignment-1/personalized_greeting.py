f9a53a752bdba0819bf3f5e7fea2b1e950184670
mayursangave5 <sangavemayur5@gmail.com>
2026-02-05 00:14:55 +0530

    Task-2

---
+++ Assignment-1/personalized_greeting.py
@@ -0,0 +1,8 @@
+first_name=input("enter your first name:-")
+last_name=input("enter your last name:-")
+
+full_name=first_name+" "+last_name
+print(f"hello,{full_name}! welcome to python program.")
+#OR
+#print(f"hello,{first_name} {last_name}! welcome to python program.")
+
